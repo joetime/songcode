f02ce5f3e6b29845c433c90d4adb7b905c30555d
joetime <joetime@gmail.com>
2016-08-31 10:43:09 -0400

    first commit

---
+++ satisfaction.cs
@@ -0,0 +1,30 @@
+
+private int? getSatisfaction(int tries) {
+    
+    try {
+        return tries / 0;
+    }
+    catch (System.Exception ex) {
+        return null;
+    }
+}
+
+while (self.activity == Activity.DRIVING && car.Radio) {
+    
+    self.information.push(null);
+    self.imagination.onFire = false;
+
+    self.satisfaction = getSatisfaction(3);
+}
+
+while (self.activity == Activity.WATCHING_TV) {
+    
+    self.shirts.white = false;
+
+    if (TV.man.cigarettes != self.cigarettes)
+        TV.man.sex = Sex.FEMALE; 
+
+    self.satisfaction = getSatisfaction(3);
+}
+
+
